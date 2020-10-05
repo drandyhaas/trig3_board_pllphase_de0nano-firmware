39527a975a78b72cce9552279366e8a55bb08c1d
mgershow <gershow@gmail.com>
2020-10-05 15:28:07 -0400

    trimming down led_4.v and rewriting to use shift registers, eliminate for loops

--- LED_4.v
+++ LED_4.v
@@ -1,11 +1,8 @@
 module LED_4(
 	input nrst,
 	input clk_lvds,
-	output reg [3:0] led,
 	input [15:0] coax_in,
 	output [15:0] coax_out,	
-	input [7:0] deadticks, input [7:0] firingticks,
-	input clk_test,
 	input clkin, input passthrough,
 	output integer histo[8], input resethist, input vetopmtlast,	//histo must have at least NBINS
 	input [NBINS-1:0] lvds_rx,
@@ -20,20 +17,12 @@ module LED_4(
 	parameter NBINS = 8;
 	
 	// for testing logic
-	reg pmt1test;
-	reg [5:0] clk1counter=0;
-	always@(posedge clk_test) begin // runs on the fast clock
-		clk1counter <= clk1counter+1;		
-		if (clk1counter == 1) pmt1test<=1; // make a test pulse
-		else pmt1test<=0;
-	end
 	
-	wire pmt1;
+	
 	//assign pmt1 = pmt1test; // pmt test input
 	assign pmt1 = coax_in[3] ||coax_in[8]; // pmt input (LVDS) || (single-ended)
 	
-	assign coax_out[0]=pmt1test; //N11 // a test pulse
-	assign coax_out[1]=clk_test; // P9 // the 4x input for test pulses
+
 	reg out1;assign coax_out[2]=out1; // A6 // the out1
 	reg out2;assign coax_out[3]=out2; // B6 // the out2
 	
@@ -41,17 +30,14 @@ module LED_4(
 	assign coax_out[4]=clkin; // F9 // the input clock that can also have its phase adjusted
 	assign coax_out[5]=clk_lvds; // unassigned // the clk for lvds that can also have its phase adjusted
 	
-	
-	assign led[0]=pmt1;
-	assign led[1]=out1;
-	assign led[2]=out2;
-	assign led[3]=1;
 		
-	reg resethist1=0, resethist2=0;
+	reg resethist1=0, resethist2=0, resetipi = 0;
 	reg [NBINS-1:0] lvds_last=0;
 	reg [NBINS-1:0] phot=0;
-	reg [7:0] j;
-	reg [7:0] k;
+   reg [NBINS-1:0] lastphot = 0;
+
+	reg [7:0] j = 0;
+	reg [7:0] k = 0;
 	
 	reg [7:0] cyclecounter;
 	reg wasphot;
@@ -60,7 +46,7 @@ module LED_4(
 	reg collision; assign coax_out[7] = collision; // n11; two photons arrived within veto window
    reg anyphot; assign coax_out[8] = anyphot; //p15
 	reg cycletoggle; assign coax_out[9] = cycletoggle; //e7
-
+	
 	always@(posedge clkin) begin
 		if (passthrough) begin
 			out1 <= pmt1;
@@ -68,11 +54,7 @@ module LED_4(
 		end
 		else begin			
 			if (vetopmtlast) begin
-				lvds_last[NBINS-1] = lvds_last[0];
-				for (j=0; j<NBINS-1; j=j+1) begin
-					lvds_last[j] = lvds_rx[j+1];
-				end
-				phot = lvds_rx & ~lvds_last;
+				phot = lvds_rx & ~((lvds_rx >> 1) || (lvds_last << (NBINS-1)));
 			end
 			else begin
 				phot = lvds_rx;
@@ -86,7 +68,8 @@ module LED_4(
 			//<= --> parallel execution (simultaneous update at the end of the clock cycle)
 			// = --> serial execution
 			
-			if (phot) begin
+			anyphot <= phot != 0;
+			if (anyphot) begin
 				if (cyclecounter < 64) begin
 					ipihist[cyclecounter] <= ipihist[cyclecounter] + 1;				
 				end 
@@ -101,32 +84,47 @@ module LED_4(
 
 			out1 <= (phot & mask1) != 0;
 			out2 <= (phot & mask2) != 0;
-			anyphot <= phot != 0;
 			
 			cycletoggle <= !cycletoggle;
 			
-			lvds_last = lvds_rx;
+			lvds_last <= lvds_rx;
 
-			resethist1<=resethist;
-			resethist2<=resethist1;
+			resethist1 <= resethist;
+			resethist2 <= resethist2 || resethist1;
+			resetipi <= resetipi || resethist1;
 			
-			
+			lastphot <= phot;
 			if (resethist2) begin
-				for (j=0; j<NBINS-1; j=j+1) begin
-					histo[j] <= 0; 
+				if (j >= NBINS) begin
+					j = 0;
+					resethist2 = 0;
+				end
+				else begin
+					histo[j] <= 0;
+					j <= j+1;
 				end
 				
-				for (k = 0; k < 64; k = k + 1) begin
+			end
+			else begin				
+				histo[0] <= histo[0] + phot[0];
+				histo[1] <= histo[1] + phot[1];
+				histo[2] <= histo[2] + phot[2];
+				histo[3] <= histo[3] + phot[3];
+				histo[4] <= histo[4] + phot[4];
+				histo[5] <= histo[5] + phot[5];
+				histo[6] <= histo[6] + phot[6];
+				histo[7] <= histo[7] + phot[7];
+			end
+			if (resetipi) begin
+				if (k >= 64) begin
+					k = 0;
+					resetipi = 0;
+				end
+				else begin
 					ipihist[k] <= 0;
-				end
-			end
-			else begin
-				
-				for (j=0; j<NBINS-1; j=j+1) begin
-					histo[j] <= histo[j] + phot[j]; 
-				end
-			end
-			
+					k <= k+1;
+				end				
+			end		
 		end		
 	end
 	
