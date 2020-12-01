5cb632ae2c64bb2842b86c658724250bfbd162a9
Marc Gershow <mhg4@nyu.edu>
2020-12-01 11:42:10 -0500

    disabled debugging outputs

---
+++ outbuf1_inst.v
@@ -0,0 +1,5 @@
+outbuf1	outbuf1_inst (
+	.datain ( datain_sig ),
+	.oe ( oe_sig ),
+	.dataout ( dataout_sig )
+	);
---
+++ outbuf2_inst.v
@@ -0,0 +1,5 @@
+outbuf2	outbuf2_inst (
+	.datain ( datain_sig ),
+	.oe ( oe_sig ),
+	.dataout ( dataout_sig )
+	);
